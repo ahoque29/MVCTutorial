46ec3f4e31628fdc88f29c88892cd3a41eb7dec9
Ashib Hoque <a.hoque29@hotmail.com>
2021-03-22 01:31:48 +0000

    Added category model

---
+++ Rocky/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Rocky.Models
+{
+	public class Category
+	{
+		[Key]
+		public int CategoryId { get; set; }
+
+		public string CategoryName { get; set; }
+
+		public int DisplayOrder { get; set; }
+
+
+
+	}
+}
