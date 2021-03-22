cd7e0daa171ae064fb512fd353960556613081ea
Ashib Hoque <a.hoque29@hotmail.com>
2021-03-22 02:33:26 +0000

    code cleanup test 2

--- Rocky/Data/ApplicationDbContext.cs
+++ Rocky/Data/ApplicationDbContext.cs
@@ -1,17 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Rocky.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Rocky.Data
 {
 	public class ApplicationDbContext : DbContext
 	{
-		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options)
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
 		{
-
 		}
 
 		public DbSet<Category> Categories { get; set; }
